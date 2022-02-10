208bbf564e2bffc5ad88d21cb4ec873b486d4125
Diogo Guilhen <contato@diogoguilhen.com.br>
2022-02-10 16:23:24 -0300

    commit master

---
+++ OriginalScrappingCartao/WebScrappingOriginal.R
@@ -0,0 +1,30 @@
+#install.packages(c("XML","rvest","xml2","read_html"), dep=T)
+#install.packages("tidyverse",dep=T)
+library("XML")
+library("rvest")
+library(rvest)
+library(tidyverse)
+
+getwd()
+
+setwd("C:/Users/sguil/OneDrive/R")
+
+getwd()
+
+
+page <- read_html('Banco Original.html') 
+ 
+tabela <- html_nodes(page,".timeline-default .ng-scope" ) %>% 
+  map_df(~{
+    data_frame(
+      Data =  html_nodes(.x,".date-timeline") %>% html_text(trim=T),
+      Estabelecimento =  html_nodes(.x,".colun01 .ng-binding") %>% html_text(trim=T),
+      Valor =  html_nodes(.x,".timeline-info .colun04 .ng-binding") %>% html_text(trim=T) 
+    )
+  }) 
+
+ tabela 
+
+write.csv(tabela, file="original2.csv",col.names = T, sep="|")
+
+
